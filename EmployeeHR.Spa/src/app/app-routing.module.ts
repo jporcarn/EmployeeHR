c68d2eb485c8c9fad072eed2836fcf356fc94898
JOSEP PORCAR NADAL <jjppnn@hotmail.com>
2021-05-22 10:07:42 +0200

    FrontEnd: Landing Page + Router outlet + Employee component

--- EmployeeHR.Spa/src/app/app-routing.module.ts
+++ EmployeeHR.Spa/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { ErrorComponent } from './error/error.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+    { path: 'employee', component: EmployeeComponent },
+    { path: 'error', component: ErrorComponent },
+    { path: '**', component: NotFoundComponent }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- EmployeeHR.Spa/src/app/app.module.ts
+++ EmployeeHR.Spa/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeComponent } from './employee/employee.component';
+import { ErrorComponent } from './error/error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    ErrorComponent,
+    NotFoundComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GridModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
