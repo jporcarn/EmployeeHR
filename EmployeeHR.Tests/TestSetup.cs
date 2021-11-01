4f739a3cd2ea0a7f23ec7d7de723fb68227f96b2
Josep Porcar <jporcar@ohpen.com>
2021-11-01 12:17:44 +0100

    Use the same setup.cs class in both Infrastructure and Domain integration tests

--- EmployeeHR.Tests/TestSetup.cs
+++ EmployeeHR.Tests/TestSetup.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeHR.Dal.Tests
+namespace EmployeeHR.Tests
 {
     [TestClass()]
     public static class TestSetup
